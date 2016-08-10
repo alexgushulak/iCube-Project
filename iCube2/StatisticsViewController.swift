1526c65fdfac785298e15ab775deb450033f0c87
JACOB Rasmussen <jacobrasmussen@jacobs-macbook-pro.local>
2016-08-09 21:12:41 -0500

    jhfd

--- iCube2/StatisticsViewController.swift
+++ iCube2/StatisticsViewController.swift
@@ -13,17 +13,25 @@ class StatisticsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.blackColor()
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(StatisticsViewController.refreshAve(_:)), name: "refresh", object: nil)
+        let aveTime = average(global.currentSolves).toString()
+        averageTime.text = aveTime
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func refreshAve(notification: NSNotification) {
+        let aveTime = average(global.currentSolves).toString()
+        averageTime.text = "\(aveTime)"
+    }
     
     @IBOutlet weak var averageTime: UILabel!
-  
+    
+    
     func updateAverage() {
-       let aveTime = average(global.solves3)
-       averageTime.text = aveTime.toString()
+        //let aveTime = average(global.solves3)
     }
+
 }
